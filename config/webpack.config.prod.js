80ccd91ad3152abdfc50c8c178e175361eb21c2d
Freddy Rangel <frederick.rangel@gmail.com>
2017-02-26 22:27:13 -0800

    09-vendor-bundle-and-caching

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,11 +1,17 @@
+const webpack           = require('webpack');
 const smartMerge        = require('webpack-merge').smart;
 const base              = require('./webpack.config.base.js');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const inline            = require('inline-manifest-webpack-plugin');
 
 module.exports = smartMerge(base, {
   devtool: 'source-map',
   plugins: [
-    new ExtractTextPlugin('style.css')
+    new ExtractTextPlugin('style.[chunkhash].css'),
+    new inline,
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
   ],
   module: {
     rules: [
